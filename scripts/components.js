a23c19e6d8518701f79e2fe792d5a1299bbf52cb
Amer Hamdan <amashamdan@gmail.com>
2016-06-15 02:57:53 +0300

    Refactor: Simplified creation of the grid process.
    Changes are made in getInitialState of Maze.
    Instead of writing a seperate for loop to create each room, an array of objects 'room' is created and stores each room's information.
    A for loop loops through the rooms array and creates rooms using each rooms information.

--- scripts/components.js
+++ scripts/components.js
@@ -56,124 +56,33 @@ var Maze = React.createClass({
 			}
 		}
 
-		for (var i = 3; i < 13; i++) {
-			for (var k = 3; k < 13; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}	
+		var rooms = [{room: 0, rowStart: 3, rowEnd: 13, colomnStart: 3, colomnEnd: 13},
+					{room: 1, rowStart: 14, rowEnd: 20, colomnStart: 3, colomnEnd: 8},
+					{room: 2, rowStart: 21, rowEnd: 27, colomnStart: 5, colomnEnd: 20},
+					{room: 3, rowStart: 1, rowEnd: 8, colomnStart: 14, colomnEnd: 21},
+					{room: 4, rowStart: 9, rowEnd: 20, colomnStart: 14, colomnEnd: 24},
+					{room: 5, rowStart: 4, rowEnd: 11, colomnStart: 25, colomnEnd: 30},
+					{room: 6, rowStart: 8, rowEnd: 15, colomnStart: 31, colomnEnd: 45},
+					{room: 7, rowStart: 2, rowEnd: 10, colomnStart: 46, colomnEnd: 54},
+					{room: 8, rowStart: 16, rowEnd: 26, colomnStart: 25, colomnEnd: 34},
+					{room: 9, rowStart: 16, rowEnd: 27, colomnStart: 35, colomnEnd: 45},
+					{room: 10, rowStart: 28, rowEnd: 37, colomnStart: 14, colomnEnd: 24},
+					{room: 11, rowStart: 28, rowEnd: 33, colomnStart: 25, colomnEnd: 37},
+					{room: 12, rowStart: 28, rowEnd: 40, colomnStart: 38, colomnEnd: 44},
+					{room: 13, rowStart: 38, rowEnd: 44, colomnStart: 9, colomnEnd: 19},
+					{room: 14, rowStart: 38, rowEnd: 49, colomnStart: 25, colomnEnd: 37},
+					{room: 15, rowStart: 41, rowEnd: 46, colomnStart: 38, colomnEnd: 50},
+					{room: 16, rowStart: 41, rowEnd: 57, colomnStart: 51, colomnEnd: 58},
+					{room: 17, rowStart: 47, rowEnd: 55, colomnStart: 41, colomnEnd: 50},
+					{room: 18, rowStart: 50, rowEnd: 55, colomnStart: 27, colomnEnd: 34},
+					{room: 19, rowStart: 45, rowEnd: 51, colomnStart: 16, colomnEnd: 24}];
 
-		for (var i = 14; i < 20; i++) {
-			for (var k = 3; k < 8; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 21; i < 27; i++) {
-			for (var k = 5; k < 20; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 1; i < 8; i++) {
-			for (var k = 14; k < 21; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 9; i < 20; i++) {
-			for (var k = 14; k < 24; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 4; i < 11; i++) {
-			for (var k = 25; k < 30; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 8; i < 15; i++) {
-			for (var k = 31; k < 45; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 2; i < 10; i++) {
-			for (var k = 46; k < 54; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 16; i < 26; i++) {
-			for (var k = 25; k < 34; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 16; i < 27; i++) {
-			for (var k = 35; k < 45; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 28; i < 37; i++) {
-			for (var k = 14; k < 24; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 28; i < 33; i++) {
-			for (var k = 25; k < 37; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 28; i < 40; i++) {
-			for (var k = 38; k < 44; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 38; i < 44; i++) {
-			for (var k = 9; k < 19; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 38; i < 49; i++) {
-			for (var k = 25; k < 37; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 41; i < 46; i++) {
-			for (var k = 38; k < 50; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 41; i < 57; i++) {
-			for (var k = 51; k < 58; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 47; i < 55; i++) {
-			for (var k = 41; k < 50; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 50; i < 55; i++) {
-			for (var k = 27; k < 34; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
-		}
-
-		for (var i = 45; i < 51; i++) {
-			for (var k = 16; k < 24; k++) {
-				squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
-			}
+		for (var room in rooms) {
+			for (var i = rooms[room].rowStart; i < rooms[room].rowEnd; i++) {
+				for (var k = rooms[room].colomnStart; k < rooms[room].colomnEnd; k++) {
+					squares[i].splice(k, 1, <div key={[i, k]} className="room"></div>);
+				}
+			}			
 		}
 
 		var passages = [[5, 13], [13, 5], [12, 13], [9,24], [20, 17], [10, 30], [9, 45], [15, 32], [15, 44], [27, 41], [30, 37], [19, 24], [39, 37], [32, 24], [37, 15], [44, 16], [49, 30], [45, 37], [43, 50], [54, 50]];
